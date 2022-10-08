0d68991e63b6e4c3e1e432a2f84ada2258a67e7f
Youssefat21 <yousefyousef4339@gmail.com>
2022-10-08 21:13:11 +0200

    fahrenheight

---
+++ Fahrenheit to centigrate/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Fahrenheit_to_centigrate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double fahrenheit, centigrate;
+            Console.WriteLine("Enter temperture with fahrenheit : ");
+            fahrenheit = double.Parse(Console.ReadLine());
+            centigrate = (fahrenheit - 32) * 5 / 9;
+            Console.WriteLine("Tempreture with centigrate : " + centigrate);
+        }
+    }
+}
